d3690a2fc804491182915c203d3b2f390e5b7f72
krvnl <jkakutina@gmail.com>
2017-12-01 00:33:17 +0300

    Добавил обработку событий на карте.

--- js/map.js
+++ js/map.js
@@ -2,6 +2,8 @@
 'use strict';
 
 (function () {
+  var ESC_KEYCODE = 27;
+  var ENTER_KEYCODE = 13;
   var IMG_WIDTH = 40;
   var IMG_HEIGHT = 40;
   var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
@@ -82,30 +84,6 @@
     return adverts;
   }
 
-  function removeUnnessaryFeatureElements(offer, cardElement) {
-    for (var i = 0; i < FEATURES.length; i++) {
-      if (offer.features.indexOf(FEATURES[i]) < 0) {
-        cardElement.querySelector('.feature--' + FEATURES[i]).remove();
-      }
-    }
-  }
-
-  function renderDefaultAdvert(advert) {
-    var offer = advert.offer;
-
-    cardElement.querySelector('h3').textContent = offer.title;
-    cardElement.querySelector('small').textContent = offer.address;
-    cardElement.querySelector('.popup__price').innerHTML = offer.price + ' &#x20bd;/ночь';
-    cardElement.querySelector('h4').textContent = TYPES_TRANSLATE[offer.type];
-    cardElement.querySelector('h4').nextElementSibling.textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
-    cardElement.querySelector('h4').nextElementSibling.nextElementSibling.textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
-    cardElement.querySelector('.popup__pictures').previousElementSibling.textContent = offer.description;
-    removeUnnessaryFeatureElements(offer, cardElement);
-    cardElement.querySelector('.popup__avatar').src = advert.author.avatar;
-
-    filtersContainerElement.appendChild(cardElement);
-  }
-
   function renderPin(advert) {
     var pinElement = pinTemplateElement.cloneNode(true);
 
@@ -125,16 +103,118 @@
     pinListElement.appendChild(fragmentElement);
   }
 
+  function onEscPress(evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      closeCard(clickedElement);
+    }
+  }
+
+  function openCard(evt) {
+    offPinActive(clickedElement);
+    clickedElement = evt.currentTarget;
+    onPinActive(clickedElement);
+
+    renderDefaultAdvert(adverts[getNumberOfPin(clickedElement)]);
+    closeIconElement = userDialogElement.querySelector('.popup__close');
+    closeIconElement.addEventListener('click', function () {
+      closeCard(clickedElement);
+    });
+
+    closeIconElement.addEventListener('keydown', function () {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        closeCard(clickedElement);
+      }
+    });
+
+    document.addEventListener('keydown', onEscPress);
+  }
+
+  function offPinActive(clickedElement) {
+    if (clickedElement) {
+      clickedElement.classList.remove('map__pin--active');
+    }
+  }
+
+  function onPinActive() {
+    clickedElement.classList.add('map__pin--active');
+  }
+
+  function getNumberOfPin(clickedElement) {
+    return clickedElement.querySelector('img').src.charAt(60) - 1;
+  }
+
+  function renderDefaultAdvert(advert) {
+    var offer = advert.offer;
+    var cardElement = cardTemplateElement.cloneNode(true);
+
+    cardElement.querySelector('h3').textContent = offer.title;
+    cardElement.querySelector('small').textContent = offer.address;
+    cardElement.querySelector('.popup__price').innerHTML = offer.price + ' &#x20bd;/ночь';
+    cardElement.querySelector('h4').textContent = TYPES_TRANSLATE[offer.type];
+    cardElement.querySelector('h4').nextElementSibling.textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
+    cardElement.querySelector('h4').nextElementSibling.nextElementSibling.textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+    cardElement.querySelector('.popup__pictures').previousElementSibling.textContent = offer.description;
+    removeUnnessaryFeatureElements(offer, cardElement);
+    cardElement.querySelector('.popup__avatar').src = advert.author.avatar;
+    filtersContainerElement.appendChild(cardElement);
+  }
+
+  function removeUnnessaryFeatureElements(offer, cardElement) {
+    for (var i = 0; i < FEATURES.length; i++) {
+      if (offer.features.indexOf(FEATURES[i]) < 0) {
+        cardElement.querySelector('.feature--' + FEATURES[i]).remove();
+      }
+    }
+  }
+
+  function closeCard(clickedElement) {
+    document.querySelector('.map__filters-container article').remove();
+    offPinActive(clickedElement);
+    document.removeEventListener('keydown', onEscPress);
+  }
+
+  function showMap() {
+    userDialogElement.classList.remove('map--faded');
+    renderAllPins(adverts);
+    var pinElements = pinListElement.querySelectorAll('#butPin');
+    for (var j = 0; j < pinElements.length; j++) {
+      pinElements[j].addEventListener('mouseup', function (evt) {
+        openCard(evt);
+      });
+
+      pinElements[j].addEventListener('keydown', function (evt) {
+        if (evt.keyCode === ENTER_KEYCODE) {
+          openCard(evt);
+        }
+      });
+    }
+  }
+
+  function showForm() {
+    var noticeFormElement = document.querySelector('.notice__form');
+    noticeFormElement.classList.remove('notice__form--disabled');
+    var formElements = noticeFormElement.querySelectorAll('.form__element');
+    for (var i = 0; i < formElements.length; i++) {
+      formElements[i].removeAttribute('disabled');
+    }
+  }
   var adverts = createAdverts();
+
   var userDialogElement = document.querySelector('.map');
-  userDialogElement.classList.remove('map--faded');
 
   var pinListElement = userDialogElement.querySelector('.map__pins');
   var pinTemplateElement = document.querySelector('template').content.querySelector('.map__pin');
-  renderAllPins(adverts);
 
+  var cardTemplateElement = document.querySelector('template').content.querySelector('.map__card');
   var filtersContainerElement = userDialogElement.querySelector('.map__filters-container');
-  var cardElement = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
-  renderDefaultAdvert(adverts[0]);
+
+  var clickedElement = null;
+  var closeIconElement = null;
+
+  var mainPinElement = document.querySelector('.map__pin--main');
+  mainPinElement.addEventListener('mouseup', function () {
+    showMap();
+    showForm();
+  });
 })();
 
