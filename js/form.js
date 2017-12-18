15fefa0e7db24d38782a1b40612f679661cc3377
krvnl <jkakutina@gmail.com>
2017-12-19 00:52:14 +0300

    Теперь вся синхронизация в модуле synchronize-fields.js

--- js/form.js
+++ js/form.js
@@ -4,13 +4,6 @@
 (function () {
   var noticeFormElement = document.querySelector('.notice__form');
 
-  var timeInElement = document.querySelector('#timein');
-  var timeOutElement = document.querySelector('#timeout');
-  var typeElement = document.querySelector('#type');
-  var priceElement = document.querySelector('#price');
-  var capacityElement = document.querySelector('#capacity');
-  var roomNumberElement = document.querySelector('#room_number');
-
   window.form = {
     showForm: function () {
       noticeFormElement.classList.remove('notice__form--disabled');
@@ -20,28 +13,6 @@
       }
     }
   };
-
-  // Автоматическое изменение полей заезда/выезда
-  timeInElement.addEventListener('change', function (event) {
-    var index = event.target.selectedIndex;
-    window.synchro.synchronizeFields(event.target, timeOutElement, index, ['12:00', '13:00', '14:00'], window.synchro.syncValues);
-  });
-  timeOutElement.addEventListener('change', function (event) {
-    var index = event.target.selectedIndex;
-    window.synchro.synchronizeFields(event.target, timeInElement, index, ['12:00', '13:00', '14:00'], window.synchro.syncValues);
-  });
-
-  // Автоматическое изменение цены за ночь
-  typeElement.addEventListener('change', function (event) {
-    var index = event.target.selectedIndex;
-    window.synchro.synchronizeFields(event.target, priceElement, index, [1000, 0, 5000, 10000], window.synchro.syncMinValues);
-  });
-
-  // Автоматическое изменение количества гостей
-  roomNumberElement.addEventListener('change', function (event) {
-    var index = event.target.selectedIndex;
-    window.synchro.synchronizeFields(event.target, capacityElement, index, ['1', '2', '3', '0'], window.synchro.syncValues);
-  });
 
   var inputValidateElements = noticeFormElement.querySelectorAll('input[type="text"], input[type="number"]');
 
--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -2,17 +2,45 @@
 'use strict';
 
 (function () {
-  window.synchro = {
-    syncValues: function (element, value) {
-      element.value = value;
-    },
-    syncMinValues: function (element, value) {
-      element.min = value;
-    },
-    synchronizeFields: function (elem1, elem2, index, range, method) {
-      var value = range[index];
-      method(elem2, value);
+  var timeInElement = document.querySelector('#timein');
+  var timeOutElement = document.querySelector('#timeout');
+  var roomNumberElement = document.querySelector('#room_number');
+  var capacityElement = document.querySelector('#capacity');
+  var typeElement = document.querySelector('#type');
+  var priceElement = document.querySelector('#price');
+
+  window.synchronizeFields = function (method, elem1, elem2, range) {
+    switch (method) {
+      case syncIndex:
+        elem1.addEventListener('change', function (event) {
+          syncIndex(elem2, event.target.selectedIndex);
+        });
+        elem2.addEventListener('change', function (event) {
+          syncIndex(elem1, event.target.selectedIndex);
+        });
+        break;
+      case syncMinValue:
+        elem1.addEventListener('change', function (event) {
+          syncMinValue(elem2, range[event.target.selectedIndex]);
+        });
+        break;
     }
   };
 
+  function syncIndex (element, index) {
+    element.selectedIndex = index;
+  }
+
+  function syncMinValue (element, value) {
+    element.min = value;
+  }
+
+  // Автоматическое изменение полей заезда/выезда
+  synchronizeFields(syncIndex, timeInElement, timeOutElement);
+
+  // Автоматическое изменение количества гостей
+  synchronizeFields(syncIndex, roomNumberElement, capacityElement);
+
+  // Автоматическое изменение цены за ночь
+  synchronizeFields(syncMinValue, typeElement, priceElement, [1000, 0, 5000, 10000]);
 })();
